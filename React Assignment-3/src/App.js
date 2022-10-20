35c100a3e1997f105dc8553699d91c57cad0a732
Savyansh <115232696+Savyansh@users.noreply.github.com>
2022-10-20 19:46:33 +0530

    Add files via upload

--- React Assignment-3/src/App.js
+++ React Assignment-3/src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
+import Design from "./designComponent/Design";
+
+// import HeaderComponent from "./components/HeaderComponent";
+// import FooterComponent from "./components/FooterComponent";
+// import ContentComponent from "./components/ContentComponent";
+import Login from "./loginComponent/Login";
+import StopWatch from "./stopwatchComponent/StopWatch";
 import TableComponent from "./tableComponent/TableComponent";
 import books from "./tableComponent/TableContent.json";
 
 function App() {
+  // const employeesArr = [];
   return (
     <>
+      <Login />
       <TableComponent bookDetails={books} />
+      <Design />
+      <StopWatch />
     </>
+    // <div className="App">
+    //   <HeaderComponent name="home"></HeaderComponent>
+
+    //   <ContentComponent employees={employeesArr}></ContentComponent>
+    //   <FooterComponent copyright="Savyansh@gmail.com"></FooterComponent>
+    // </div>
   );
 }
 
